2f51fcfbef8b6cbc4f79e4a01bad08b324f0e416
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-07-19 22:26:20 +0000

    * tests/pulse_shape_test.{cpp,ref}: fixed different floating-point   numbers' precision in `cout' output problem by using `setprecision()'   manipulator
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@22 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- tests/pulse_shape_test.cpp
+++ tests/pulse_shape_test.cpp
@@ -1,15 +1,15 @@
-#include "itpp/itcomm.h"
+#include <iomanip>
+#include <itpp/itcomm.h>
 
-using std::cout;
-using std::endl;
-using std::complex;
+using namespace std;
 using namespace itpp;
 
 int main()
 {
+  cout.setf(ios::fixed);
   cout << "======================================" << endl;
-  cout << "    Test of pulse shaping routines" << endl;
-  cout << "======================================" << endl;
+  cout << "    Test of pulse shaping routines    " << endl;
+  cout << "======================================" << endl << endl;
 
   BPSK bpsk;
   vec symbols, samples, rsymbols;
@@ -20,43 +20,45 @@ int main()
   samples = rrc_tx.shape_symbols(symbols);
   rsymbols = rrc_rx.shape_samples(samples);
 
-  cout << "============== Root Raised Cosine ===================" << endl;
-  cout << "pulse, RRC=" << rrc_tx.get_pulse_shape() << endl;
-  cout << "symbols=" << symbols << endl;
-  cout << "samples=" << samples << endl;
-  cout << "received symbols=" << rsymbols << endl;
+ 	cout << "*** Root Raised Cosine; real input ***" << endl << endl;
+  cout << "pulse, RRC = " << setprecision(6) << rrc_tx.get_pulse_shape() 
+    << endl << endl;
+  cout << "symbols = " << setprecision(0) << symbols << endl << endl;
+  cout << "samples = " << setprecision(6) << samples << endl << endl;
+  cout << "received symbols =" << setprecision(6) << rsymbols << endl << endl;
 
   samples = rc_tx.shape_symbols(symbols);
 
-  cout << "============== Raised Cosine ===================" << endl;
-  cout << "pulse, RC=" << rc_tx.get_pulse_shape() << endl;
-  cout << "symbols=" << symbols << endl;
-  cout << "samples=" << samples << endl;
-
+  cout << "*** Raised Cosine; real input ***" << endl << endl;
+  cout << "pulse, RC = " << setprecision(6) << rc_tx.get_pulse_shape() 
+    << endl << endl;
+  cout << "symbols = " << setprecision(0) << symbols << endl << endl;
+  cout << "samples = " << setprecision(6) << samples << endl << endl;
 
   QPSK qpsk;
   cvec csymbols, csamples, crsymbols;
   Root_Raised_Cosine<complex<double> > crrc_tx(0.5), crrc_rx(0.5);
   Raised_Cosine<complex<double> > crc_tx(0.5);
 
-
   qpsk.modulate_bits(randb(40), csymbols);
   csamples = crrc_tx.shape_symbols(csymbols);
   crsymbols = crrc_rx.shape_samples(csamples);
 
-  cout << "============== Root Raised Cosine ===================" << endl;
-  cout << "pulse, RRC=" << crrc_tx.get_pulse_shape() << endl;
-  cout << "symbols=" << csymbols << endl;
-  cout << "samples=" << csamples << endl;
-  cout << "received symbols=" << crsymbols << endl;
-
+  cout << "*** Root Raised Cosine; complex input ***" << endl << endl;
+  cout << "pulse, RRC = " << setprecision(6) << crrc_tx.get_pulse_shape() 
+    << endl << endl;
+  cout << "symbols = " << setprecision(0) << csymbols << endl << endl;
+  cout << "samples = " << setprecision(6) << csamples << endl << endl;
+  cout << "received symbols = " << setprecision(6) << crsymbols << endl 
+    << endl;
 
   csamples = crc_tx.shape_symbols(csymbols);
 
-  cout << "============== Raised Cosine ===================" << endl;
-  cout << "pulse, RC=" << crc_tx.get_pulse_shape() << endl;
-  cout << "symbols=" << csymbols << endl;
-  cout << "samples=" << csamples << endl;
+  cout << "*** Raised Cosine; complex input ***" << endl << endl;
+  cout << "pulse, RC = " << setprecision(6) << crc_tx.get_pulse_shape() 
+    << endl << endl;
+  cout << "symbols = " << setprecision(0) << csymbols << endl << endl;
+  cout << "samples = " << setprecision(6) << csamples << endl << endl;
 
   return 0;
 }
