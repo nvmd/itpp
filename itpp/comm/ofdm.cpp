c2796a56dd7cac3732e362fce43b61abadd99875
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-08-27 11:25:28 +0000

    * itpp/comm/ofdm.cpp: fixed a bug in the oversampling of the OFDM   class (bugfix submitted by Thomas Eriksson)
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@33 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/comm/ofdm.cpp
+++ itpp/comm/ofdm.cpp
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------*
- *                                   IT++			             *
+ *                                   IT++                                    *
  *---------------------------------------------------------------------------*
- * Copyright (c) 1995-2002 by Tony Ottosson, Thomas Eriksson, Pål Frenger,   *
+ * Copyright (c) 1995-2005 by Tony Ottosson, Thomas Eriksson, Pål Frenger,   *
  * Tobias Ringström, and Jonas Samuelsson.                                   *
  *                                                                           *
  * Permission to use, copy, modify, and distribute this software and its     *
@@ -37,25 +37,26 @@ namespace itpp {
 
   void OFDM::set_parameters(const int inNfft, const int inNcp, const int inNupsample){
     it_assert(inNfft >= 2, "OFDM: Nfft must be >=2.");
-    it_assert(inNcp >=0 && inNcp <= inNfft, "OFDM: Ncp must be >=0 and <=Nfft.");
-    it_assert(inNupsample >=1 && inNupsample <= 100, "OFDM: Ncp must be >=1 and <=100.");
+    it_assert(inNcp >= 0 && inNcp <= inNfft, "OFDM: Ncp must be >=0 and <=Nfft.");
+    it_assert(inNupsample >= 1 && inNupsample <= 100, "OFDM: Ncp must be >=1 and <=100.");
     Nfft = inNfft;
     Ncp = inNcp;  
     Nupsample = inNupsample;
-    norm_factor = sqrt(double(Nupsample*Nfft*Nfft)/(Nfft+Ncp));
+    norm_factor = sqrt(static_cast<double>(Nupsample*Nfft*Nfft) / (Nfft+Ncp));
     setup_done = true;
   }
 
   void OFDM::modulate(const cvec &input, cvec &output){
     it_assert(setup_done == true, "OFDM::modulate: You must set the length of the FFT and the cyclic prefix!");
-    const int N = input.length()/Nfft;
+    const int N = input.length() / Nfft;
     it_assert(N*Nfft == input.length(), "OFDM::modulate: Length of input vector is not a multiple of Nfft.");
 
     output.set_length(Nupsample*N*(Nfft+Ncp));
     cvec outtemp(Nfft);
 
-    for(int i=0; i<N; i++) {
-      outtemp = ifft(input.mid(i*Nfft, Nfft),Nupsample*Nfft)*norm_factor;
+    for (int i = 0; i < N; i++) {
+			outtemp = ifft(concat(input.mid(i*Nfft, Nfft/2), zeros(Nfft*(Nupsample-1)), 
+														input.mid(i*Nfft+Nfft/2, Nfft/2))) * norm_factor;
       output.replace_mid(Nupsample*(Nfft+Ncp)*i, concat(outtemp.right(Nupsample*Ncp), outtemp));
     }
   }
@@ -69,13 +70,15 @@ namespace itpp {
 
   void OFDM::demodulate(const cvec& input, cvec &output){
     it_assert(setup_done == true, "OFDM::demodulate: You must set the length of the FFT and the cyclic prefix!");
-    const int N = input.length()/(Nfft+Ncp)/Nupsample;
+    const int N = input.length() / (Nfft+Ncp) / Nupsample;
     it_assert(Nupsample*N*(Nfft+Ncp) == input.length(), "OFDM: Length of input vector is not a multiple of Nfft+Ncp.");
 
     output.set_length(N*Nfft);
     // normalize also taking the energy loss into the cyclic prefix into account
-    for(int i =0; i<N; i++) 
-      output.replace_mid(Nfft*i, fft(input.mid(Nupsample*(i*(Nfft+Ncp)+Ncp), Nupsample*Nfft)).left(Nfft)/norm_factor);
+    for (int i = 0; i < N; i++) {
+			cvec x = fft(input.mid(Nupsample*(i*(Nfft+Ncp)+Ncp), Nupsample*Nfft));
+			output.replace_mid(Nfft*i, concat(x.left(Nfft/2), x.right(Nfft/2)) / norm_factor);
+    }
   }
 
   cvec OFDM::demodulate(const cvec &input){
