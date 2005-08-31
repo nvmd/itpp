585cbd2bad53f9f1b9cfe9359b39c9dcdd0d7e81
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-08-31 07:51:46 +0000

    * Support for libharu removed definitely. * autogen.sh: script modifications based on autogen.sh from Gaim   project * TODO list updated
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@37 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,30 @@
 #!/bin/sh
-autoreconf --install --verbose
+
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+	echo "You must have libtool installed to compile IT++";
+	echo;
+	exit;
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo "You must have automake installed to compile IT++";
+	echo;
+	exit;
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo "You must have autoconf installed to compile IT++";
+	echo;
+	exit;
+}
+
+echo "Generating configuration files for IT++, please wait...."
+echo;
+
+echo n | libtoolize --copy --force || exit;
+aclocal || exit;
+#autoheader || exit;
+automake --add-missing --copy;
+autoconf || exit;
+automake || exit;
+#./configure $@
