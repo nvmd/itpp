9454be29433f5048a1a0e2e7f642cc615de049ca
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2005-10-12 08:16:18 +0000

    * itpp/base/stat.h: added `within_tolerance' function for double   and complex<double> arguments and fixed minor documentation errors
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@53 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/base/stat.h
+++ itpp/base/stat.h
@@ -449,26 +449,38 @@ namespace itpp {
   }
 
 
-  //! Return true if the input vector \c x is elementwise within the tolerance \c tol of the reference vector \xref
-  inline bool within_tolerance(const vec &x, const vec &xref, const double tol = 1e-15)
+  //! Return true if the input value \c x is within the tolerance \c tol of the reference value \c xref
+  inline bool within_tolerance(double x, double xref, double tol = 1e-15)
+  {
+    return ( fabs(x-xref) <= tol ) ? true : false; 
+  }
+
+  //! Return true if the input value \c x is within the tolerance \c tol of the reference value \c xref
+  inline bool within_tolerance(std::complex<double> x, std::complex<double> xref, double tol = 1e-15)
+  {
+    return ( abs(x-xref) <= tol ) ? true : false; 
+  }
+
+  //! Return true if the input vector \c x is elementwise within the tolerance \c tol of the reference vector \c xref
+  inline bool within_tolerance(const vec &x, const vec &xref, double tol = 1e-15)
   {
     return ( max(abs(x-xref)) <= tol ) ? true : false; 
   }
 
-  //! Return true if the input vector \c x is elementwise within the tolerance \c tol of the reference vector \xref
-  inline bool within_tolerance(const cvec &x, const cvec &xref, const double tol = 1e-15)
+  //! Return true if the input vector \c x is elementwise within the tolerance \c tol of the reference vector \c xref
+  inline bool within_tolerance(const cvec &x, const cvec &xref, double tol = 1e-15)
   {
     return ( max(abs(x-xref)) <= tol ) ? true : false; 
   }
 
-  //! Return true if the input matrix \c X is elementwise within the tolerance \c tol of the reference matrix \Xref
-  inline bool within_tolerance(const mat &X, const mat &Xref, const double tol = 1e-15)
+  //! Return true if the input matrix \c X is elementwise within the tolerance \c tol of the reference matrix \c Xref
+  inline bool within_tolerance(const mat &X, const mat &Xref, double tol = 1e-15)
   {
     return ( max(max(abs(X-Xref))) <= tol ) ? true : false; 
   }
 
-  //! Return true if the input matrix \c X is elementwise within the tolerance \c tol of the reference matrix \Xref
-  inline bool within_tolerance(const cmat &X, const cmat &Xref, const double tol = 1e-15)
+  //! Return true if the input matrix \c X is elementwise within the tolerance \c tol of the reference matrix \c Xref
+  inline bool within_tolerance(const cmat &X, const cmat &Xref, double tol = 1e-15)
   {
     return ( max(max(abs(X-Xref))) <= tol ) ? true : false; 
   }
