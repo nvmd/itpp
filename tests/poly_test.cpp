8d3364e3e542774866b910e78a561f6a9b04f43a
tonyottosson <tonyottosson@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-12-09 14:14:36 +0000

    Added polynomial support
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@84 f17a810d-92cc-4fca-aeb8-6e32867107aa

---
+++ tests/poly_test.cpp
@@ -0,0 +1,75 @@
+#include <itpp/itbase.h>
+
+using std::cout;
+using std::endl;
+using std::complex;
+using namespace itpp;
+
+#ifdef NO_LAPACK
+#define __THIS_PROGRAM_WILL_NOT_RUN__
+#endif
+
+#ifdef NO_CBLAS
+#define __THIS_PROGRAM_WILL_NOT_RUN__
+#endif
+
+#ifdef __THIS_PROGRAM_WILL_NOT_RUN__
+int main() { cout << "LAPACK and CBLAS is needed for this test program" << endl; }
+#else
+
+int main()
+{
+
+  cout << "===================================" << endl;
+  cout << "    Test of polynomial routines" << endl;
+  cout << "===================================" << endl;
+
+  {
+    cout << "Real polynomials" << endl;
+    vec r = randn(3);
+    vec p = poly(r);
+    cvec r2 = roots(p);
+    cout << "Roots, r = " << r << endl;
+    cout << "Polynomial, p = " << p << endl;
+    cout << "r = roots(p) = " << r2 << endl;
+
+    r = randn(7);
+    p = poly(r);
+    r2 = roots(p);
+    cout << "Roots, r = " << r << endl;
+    cout << "Polynomial, p = " << p << endl;
+    cout << "r = roots(p) = " << r2 << endl;
+
+    vec x = randn(9);
+    vec y = polyval(p, x);
+    cout << "x = " << x << endl;
+    cout << "polyval(p, x) = " << y << endl;
+  }
+
+  {
+    cout << "Complex polynomials" << endl;
+    cvec r = randn_c(3);
+    cvec p = poly(r);
+    cvec r2 = roots(p);
+    cout << "Roots, r = " << r << endl;
+    cout << "Polynomial, p = " << p << endl;
+    cout << "r = roots(p) = " << r2 << endl;
+
+    r = randn_c(7);
+    p = poly(r);
+    r2 = roots(p);
+    cout << "Roots, r = " << r << endl;
+    cout << "Polynomial, p = " << p << endl;
+    cout << "r = roots(p) = " << r2 << endl;
+
+    cvec x = randn_c(9);
+    cvec y = polyval(p, x);
+    cout << "x = " << x << endl;
+    cout << "polyval(p, x) = " << y << endl;
+  }
+
+  return 0;
+
+}
+
+#endif
