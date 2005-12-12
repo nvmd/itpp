9ab3aea2f9c1f5a003792b28c553953d53d292f7
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-12-12 22:47:12 +0000

    * itpp/itconfig.h: Doxygen modules moved from `alpha_modules.h'   into this file * itpp/Makefile.am, doc/Makefile.am, doc/doxygen_html.cfg.in:   minor updates in lists of `*.h' and `*.cpp' sources
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@90 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/itconfig.h
+++ itpp/itconfig.h
@@ -44,6 +44,53 @@
 #define __WIN32__
 #endif
 
+namespace itpp {
+
+  /*! \defgroup parser Argument Parser */
+  /*! \defgroup audio Audio */
+  /*! \defgroup besselfunctions Bessel Functions */
+  /*! \defgroup channels Communication Channel Models */
+  /*! \defgroup convertfunc Conversion Functions */
+  /*! \defgroup determinant Determinant */
+  /*! \defgroup detsource Deterministic Sources */
+  /*! \defgroup diag Diagonal Matrices and Functions of Diagonals */
+  /*! \defgroup modulators Digital Modulation */
+  /*! \defgroup dct Discrete Cosine Transforms (DCT) */
+  /*! \defgroup errorfunc Error Functions */
+  /*! \defgroup errorhandlingfunc Error Handling Functions */
+  /*! \defgroup fft Fast Fourier Transforms (FFT) */
+  /*! \defgroup fht Fast Hadamard Transforms */
+  /*! \defgroup fastica Fast Independent Component Analysis */
+  /*! \defgroup filters Filter Classes and Functions */
+  /*! \defgroup fixtypes Fixed-Point Data Types */
+  /*! \defgroup fec Forward Error Correcting Codes */
+  /*! \defgroup matrix_functions Functions on Matrices */
+  /*! \defgroup hypfunc Hyperbolic Functions */
+  /*! \defgroup image Image Functions and Classes */
+  /*! \defgroup interl Interleavers */
+  /*! \defgroup inverse Inverse Matrix */
+  /*! \defgroup itfile IT++ File Format */
+  /*! \defgroup logexpfunc Logarithmic and Exponential Functions */
+  /*! \defgroup lpc LPC-related Functions */
+  /*! \defgroup matrixdecomp Matrix Decompositions */
+  /*! \defgroup miscfunc Miscellaneous Functions */
+  /*! \defgroup integration Numerical Integration */
+  /*! \defgroup randgen Random Number Generation */
+  /*! \defgroup reshaping Reshaping of Vectors and Matrices */
+  /*! \defgroup sigproc Signal Processing Functions */
+  /*! \defgroup linearequations Solving Linear Equation Systems */
+  /*! \defgroup sourcecoding Source Coding Routines */
+  /*! \defgroup specmat Special Matrices */
+  /*! \defgroup statistics Statistics */
+  /*! \defgroup timers Timers */
+  /*! \defgroup trifunc Trigonometric Functions */
+  /*! \defgroup upsample Upsampling of Vectors and Matrices */
+  /*! \defgroup windfunc Windows */
+  /*! \defgroup mexfiles Writing Matlab Mex-files Using IT++ */
+
+} // namespace itpp
+
+
 namespace std {
 
   //! Output stream operator for complex numbers
