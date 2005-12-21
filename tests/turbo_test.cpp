c27885a0036e53fa705406fda6d69b96907ff38a
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2005-12-21 14:22:22 +0000

    Minor fix and cosmetic changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@126 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- tests/turbo_test.cpp
+++ tests/turbo_test.cpp
@@ -1,7 +1,40 @@
+/*!
+ * \file 
+ * \brief Turbo encoder/decoder class test program
+ * \author Pal Frenger
+ *
+ * $Date$
+ * $Revision$
+ *
+ * -------------------------------------------------------------------------
+ *
+ * IT++ - C++ library of mathematical, signal processing, speech processing,
+ *        and communications classes and functions
+ *
+ * Copyright (C) 1995-2005  (see AUTHORS file for a list of contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * -------------------------------------------------------------------------
+ */
+
 #include <itpp/itcomm.h>
 
-using namespace std;
 using namespace itpp;
+using namespace std;
+
 
 int main()
 {
@@ -15,30 +48,33 @@ int main()
     string metric = "LOGMAX";
     double logmax_scale_factor = 0.7;
     bool adaptive_stop = true;
-    turbo.set_parameters(gen, gen, constraint_length, interleaver_sequence, iterations, metric, 
-			 logmax_scale_factor, adaptive_stop);
+    turbo.set_parameters(gen, gen, constraint_length, interleaver_sequence, 
+			 iterations, metric, logmax_scale_factor, 
+			 adaptive_stop);
     int num_blocks = 50;
 
     vec EbN0db = "0.0 0.5 1.0 1.5 2.0";
     double A = 1.0;
     double Ts = 1.0;
-    double Ec = A*A*Ts;
-    double r = double(block_length) / (3.0 * block_length + 8.0); //8 tailbits are added....
+    double Ec = A * A * Ts;
+    double r = block_length / (3.0 * block_length + 8.0); // 8 tailbits
     double Eb = Ec / r;
-    vec EbN0 = pow(double(10.0),EbN0db/double(10.0));
-    vec N0 = Eb * pow(EbN0,-1.0);
-    vec sigma2 = N0/2;
+    vec EbN0 = pow(10.0, 0.1 * EbN0db);
+    vec N0 = Eb * pow(EbN0, -1.0);
+    vec sigma2 = N0 / 2;
     vec ber(EbN0db.length()); ber.clear();
     vec avg_nrof_iterations(EbN0db.length()); avg_nrof_iterations.clear();
     ivec nrof_used_iterations;
     int i;
 
-    vec rec_systematic, rec_parity, rec_parity1, rec_parity2, extrinsic_input, extrinsic_output, received, symbols;
-    bvec input, parity1, parity2, parity_punct, tail1, output1, output2, transmitted, uncoded_bits;
+    vec rec_systematic, rec_parity, rec_parity1, rec_parity2, extrinsic_input, 
+      extrinsic_output, received, symbols;
+    bvec input, parity1, parity2, parity_punct, tail1, output1, output2, 
+      transmitted, uncoded_bits;
     bvec coded_bits, decoded_bits;
 
     Normal_RNG noise_src;
-    RNG_reset(12345);//Note: RNG_randomize(); will randomize the random sources instead
+    RNG_reset(12345);
 
     BPSK bpsk;
     BERC berc;
@@ -55,12 +91,13 @@ int main()
     cout << "  Turbo encoder rate 1/3 (plus tail bits)" << endl;
     cout << "=============================================" << endl;
 
-    ivec err(EbN0db.length()), cor(EbN0db.length());
-    for (i=0; i<EbN0db.length(); i++) {
+    vec err(EbN0db.length());
+    vec cor(EbN0db.length());
 
+    for (i = 0; i < EbN0db.length(); i++) {
 	cout << "Now simulating EbN0db = " << EbN0db(i) << endl;
 
-	noise_src.setup(0.0,sigma2(i));
+	noise_src.setup(0.0, sigma2(i));
 	turbo.set_awgn_channel_parameters(Ec, N0(i));
 	input = randb(block_length * num_blocks);
 
@@ -74,10 +111,12 @@ int main()
 	err(i) = berc.get_errors();
 	cor(i) = berc.get_corrects();
 	ber(i) = berc.get_errorrate();
-	avg_nrof_iterations(i) = double(sum(nrof_used_iterations)) / double(length(nrof_used_iterations));
+	avg_nrof_iterations(i) = static_cast<double>(sum(nrof_used_iterations)) 
+	  / length(nrof_used_iterations);
     }
 
-    cout << "Average numer of iterations used = " << avg_nrof_iterations << endl;
+    cout << "Average numer of iterations used = " << avg_nrof_iterations 
+	 << endl;
     cout << "err = " << err << endl;
     cout << "cor = " << cor << endl;
     cout << "ber = " << ber << endl;
