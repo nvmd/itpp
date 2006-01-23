d0d6ce3bfb2c6878c89b439c7eec7a203b30f613
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2006-01-23 15:25:59 +0000

    Merged recent fixes from the itpp-3-9-branch (see ChangeLog for more info)
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@231 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/base/converters.cpp
+++ itpp/base/converters.cpp
@@ -252,7 +252,7 @@ namespace itpp {
 
   bvec dec2bin(int index, bool msb_first)
   {
-    int length = needed_bits(index)+1;
+    int length = needed_bits(index);
     int i, bintemp = index;
     bvec temp(length);
 
@@ -270,7 +270,7 @@ namespace itpp {
   void dec2bin(int index, bvec &v)
   {
     int i, bintemp = index;
-    v.set_size(needed_bits(index)+1, false);
+    v.set_size(needed_bits(index), false);
 
     for (i=v.size()-1; i>=0; i--) {
       v(i) = bin(bintemp & 1);
