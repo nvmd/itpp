78e9c05d72c575cf8b439e8a5f3e60490edb1034
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2006-02-06 21:47:06 +0000

    Bug-fixes in FFT/IFFT functions based on ACML library. Temporary `comm' vectors are now allocated dynamically when neccessary. This fixes segmentation faults in sigfun and filter_design test programs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@282 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/base/transforms.cpp
+++ itpp/base/transforms.cpp
@@ -51,7 +51,6 @@
 #include <itpp/base/matfunc.h>
 #include <itpp/base/transforms.h>
 #include <itpp/base/elmatfunc.h>
-//#include <iostream>
 
 
 namespace itpp { 
@@ -188,53 +187,62 @@ namespace itpp {
   void fft(const cvec &in, cvec &out) 
   {
     static int N = 0;
-    static cvec comm(5 * in.size() + 100); 
+    static cvec *comm_ptr = NULL;
     int info;
     out.set_size(in.size(), false);
 
     if (N != in.size()) {
       N = in.size();
+      if (comm_ptr != NULL)
+	delete comm_ptr;
+      comm_ptr = new cvec(5 * in.size() + 100);
       zfft1dx(0, 1.0, false, N, (doublecomplex *)in._data(), 1, 
-	      (doublecomplex *)out._data(), 1, (doublecomplex *)comm._data(), 
-	      &info);
+	      (doublecomplex *)out._data(), 1, 
+	      (doublecomplex *)comm_ptr->_data(), &info);
     }
     zfft1dx(-1, 1.0, false, N, (doublecomplex *)in._data(), 1, 
-	    (doublecomplex *)out._data(), 1, (doublecomplex *)comm._data(), 
-	    &info);
+	    (doublecomplex *)out._data(), 1, 
+	    (doublecomplex *)comm_ptr->_data(), &info);
   }
 
   void ifft(const cvec &in, cvec &out) 
   {
     static int N = 0;
-    static cvec comm(5 * in.size() + 100); 
+    static cvec *comm_ptr = NULL;
     int info;
     out.set_size(in.size(), false);
 
     if (N != in.size()) {
       N = in.size();
+      if (comm_ptr != NULL)
+	delete comm_ptr;
+      comm_ptr = new cvec(5 * in.size() + 100);
       zfft1dx(0, 1.0/N, false, N, (doublecomplex *)in._data(), 1, 
-	      (doublecomplex *)out._data(), 1, (doublecomplex *)comm._data(), 
-	      &info);
+	      (doublecomplex *)out._data(), 1, 
+	      (doublecomplex *)comm_ptr->_data(), &info);
     }
     zfft1dx(1, 1.0/N, false, N, (doublecomplex *)in._data(), 1, 
-	    (doublecomplex *)out._data(), 1, (doublecomplex *)comm._data(),
-	    &info);
+	    (doublecomplex *)out._data(), 1, 
+	    (doublecomplex *)comm_ptr->_data(), &info);
   }
 
   void fft_real(const vec &in, cvec &out)
   {
     static int N = 0;
     static double factor = 0;
-    static vec comm(5 * in.size() + 100); 
+    static vec *comm_ptr = NULL;
     int info;
     vec out_re = in;
 
     if (N != in.size()) {
       N = in.size();
       factor = std::sqrt(static_cast<double>(N));
-      dzfft(0, N, out_re._data(), comm._data(), &info);
+      if (comm_ptr != NULL)
+	delete comm_ptr;
+      comm_ptr = new vec(5 * in.size() + 100);
+      dzfft(0, N, out_re._data(), comm_ptr->_data(), &info);
     }
-    dzfft(1, N, out_re._data(), comm._data(), &info);
+    dzfft(1, N, out_re._data(), comm_ptr->_data(), &info);
 
     // Normalise output data
     out_re *= factor;
@@ -252,7 +260,7 @@ namespace itpp {
   {
     static int N = 0;
     static double factor = 0;
-    static vec comm(5 * in.size() + 100); 
+    static vec *comm_ptr = NULL;
     int info;
 
     // Convert Matlab's complex input to the real Hermitian form
@@ -263,9 +271,12 @@ namespace itpp {
     if (N != in.size()) {
       N = in.size();
       factor = 1.0 / std::sqrt(static_cast<double>(N));
-      zdfft(0, N, out._data(), comm._data(), &info);
+      if (comm_ptr != NULL)
+	delete comm_ptr;
+      comm_ptr = new vec(5 * in.size() + 100);
+      zdfft(0, N, out._data(), comm_ptr->_data(), &info);
     }
-    zdfft(1, N, out._data(), comm._data(), &info);
+    zdfft(1, N, out._data(), comm_ptr->_data(), &info);
     out.set_subvector(1, reverse(out(1, N-1)));
 
     // Normalise output data
