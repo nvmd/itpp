eb13b2cd4a3d5345bfc4404bcff6a79bfca5edd3
tonyottosson <tonyottosson@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2006-02-20 13:16:00 +0000

    Added a list of main features to documentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@320 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- doc/local/sources.mk
+++ doc/local/sources.mk
@@ -2,6 +2,7 @@ doc_local_sources = \
 	$(top_srcdir)/doc/local/codingrules.doc \
 	$(top_srcdir)/doc/local/copyright.doc \
 	$(top_srcdir)/doc/local/documentation.doc \
+	$(top_srcdir)/doc/local/features.doc \
 	$(top_srcdir)/doc/local/help_wanted.doc \
 	$(top_srcdir)/doc/local/installation.doc \
 	$(top_srcdir)/doc/local/linking.doc \
