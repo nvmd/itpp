d5bf203fbc43bc8b4c2cffd610c2d7b00543880e
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2006-03-21 15:19:10 +0000

    Added templated Histogram class provided by Andy Panov. This closes feature request [1451288].
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@379 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/base/stat.h
+++ itpp/base/stat.h
@@ -1,7 +1,7 @@
 /*!
  * \file
  * \brief Definitions of statistics functions and classes
- * \author Tony Ottosson, Johan Bergman and Adam Piatyszek
+ * \author Tony Ottosson, Johan Bergman, Adam Piatyszeka and Andy Panov
  * 
  * $Date$
  * $Revision$
@@ -122,6 +122,172 @@ namespace itpp {
   };
 
 
+  // ------------------------------------------------------------------------
+
+  /*! 
+    \brief Histogram computation class
+    \author Andy Panov
+   
+    The Histogram class counts the number of observations of arbitrary
+    numerical types that fall into specified bins. Centers of the leftmost
+    and rightmost bin along with a total number of bins are passed to a
+    histogram object as constructor parameters. Histogram counters are
+    updated when calling update() method. It is possible to access bin
+    counters and bin interval parameters for all bins at once or separately
+    for each bin.
+      
+    Example:
+    \code
+    // Create histogram with 100 bins spanning from 0 to 99 (leftmost bin is 
+    // centered at 0, rightmost bin is centerd at 99). 
+    Histogram<double> hist(0, 99, 100);
+      
+    // Compute histogram of 100 random variables taken from normal distribution
+    hist.update(randn(100));
+   
+    // Get position of bin number 5
+    double bin5_center = hist.get_bin_center(5);
+    // Get corresponding bin counter
+    int bin5_counter = hist.get_bin(5);
+    // Get bin 5 left boundary:
+    double bin5_left = hist.get_bin_left(5);
+   
+    // compute PDF & CDF of experimental data
+    vec my_data_pdf = hist.get_pdf();
+    vec my_data_cdf = hist.get_cdf();
+    \endcode
+   */
+  template<typename Num_T>
+  class Histogram {
+  public:
+    //! Default constructor. Constructs histogram with 100 bins spanning
+    //! values from 0 to 99 by default.
+    Histogram(Num_T from = Num_T(0), Num_T to = Num_T(99), int n_bins = 100);
+    //! Default destructor
+    ~Histogram() {};
+    
+    //! Histogram update
+    void update(Num_T value);
+    void update(Vec<Num_T> values);
+    void update(Mat<Num_T> values);
+    
+    //! Bins reset, so accumulation can be restarted
+    void reset() { trials_cnt = 0; bins.zeros(); };
+    //! Access to single bin counter
+    int get_bin(int ix) const { return bins(ix); };
+    //! Access to histogram as a vector
+    ivec get_bins() const { return bins; };
+    //! Access to bin center values (all bins)
+    Vec<Num_T> get_bin_centers() const { return center_vals; };
+    //! Access to bin center (single bin)
+    Num_T get_bin_center(int ix) const { return center_vals(ix); };
+    //! Access to left boundary of bin intervals (all bins)
+    Vec<Num_T> get_bin_lefts() const { return lo_vals; };
+    //! Access to left boundary of single bin 
+    Num_T get_bin_left(int ix) const { return lo_vals(ix); };
+    //! Access to right boundary of bin intervals (all bins)
+    Vec<Num_T> get_bin_rights() const { return hi_vals; };
+    //! Access to right boundary of single bin 
+    Num_T get_bin_right(int ix) const { return hi_vals(ix); };
+    
+    //! Experimental Probability Density Function (PDF) computation
+    vec get_pdf() const;
+    //! Experimental Cumulative Density Function (CDF) computation	
+    vec get_cdf() const;
+    
+    //! Current number of bins
+    int bins_num() const { return num_bins; };
+    //! Current trials counter
+    int trials_num() const {return trials_cnt;};
+  
+  private:
+    //! Number of bins
+    int num_bins;
+    //! Step between bins
+    Num_T step;
+    //! Low boundaries of histogram bins
+    Vec<Num_T> lo_vals;
+    //! Upper boundaries of histogram bins
+    Vec<Num_T> hi_vals;
+    //! Bin centers
+    Vec<Num_T> center_vals; 
+    //! Bins storage
+    ivec bins;
+    //! Number of processed samples
+    int trials_cnt;
+  };
+
+  template<class Num_T>
+  inline Histogram<Num_T>::Histogram(Num_T from, Num_T to, int n_bins): 
+    num_bins(n_bins), lo_vals(n_bins),	hi_vals(n_bins), center_vals(n_bins),
+    bins(n_bins), trials_cnt(0)
+  {
+    step = (to - from) / (num_bins - 1);
+    center_vals = linspace(from, to, num_bins);
+    lo_vals = center_vals - step/2; 
+    hi_vals = center_vals + step/2; 
+    reset();
+  }
+	
+  template<class Num_T>
+  inline void Histogram<Num_T>::update(Num_T value) 
+  {		
+    // search for the corresponding bin using dichotomy approach
+    int start = 0;
+    int end = num_bins - 1;
+    int test = (start + end) / 2;
+
+    while (start < end) {
+      if (value < lo_vals(test))
+	end = test - 1;
+      else if (value >= hi_vals(test))
+	start = test + 1;
+      else
+	break;
+      test = (start + end) / 2;
+    };
+
+    bins(test)++;
+    trials_cnt++;
+  }
+
+ template<class Num_T>
+ inline void Histogram<Num_T>::update(Vec<Num_T> values) 
+ {		
+   for (int i = 0; i < values.length(); i++)
+     update(values(i));
+ }
+
+ template<class Num_T>
+ inline void Histogram<Num_T>::update(Mat<Num_T> values) 
+ {		
+   for (int i = 0; i < values.rows(); i++)
+     for (int j = 0; j < values.cols(); j++)
+       update(values(i,j));            
+ }
+
+  template<class Num_T>
+  inline vec Histogram<Num_T>::get_pdf() const 
+  {
+    vec pdf(num_bins);
+    for (int j = 0; j < num_bins; j++)
+      pdf(j) = static_cast<double>(bins(j)) / trials_cnt;
+    return pdf;
+  }
+
+  template<class Num_T>
+  inline vec Histogram<Num_T>::get_cdf() const 
+  {
+    ivec tmp = cumsum(bins);			
+    vec cdf(num_bins);
+    for (int j = 0; j < num_bins; j++)
+      cdf(j) = static_cast<double>(tmp(j)) / trials_cnt;		
+    return cdf;
+  }
+
+  // ------------------------------------------------------------------------
+
+  
   //! Maximum value of vector
   template<class T>
     T max(const Vec<T> &v)
--- tests/stat_test.cpp
+++ tests/stat_test.cpp
@@ -1,39 +1,75 @@
 /*!
-* \file 
-* \brief Statistical routines test program
-* \author Tony Ottosson and Adam Piatyszek
-*
-* $Date$
-* $Revision$
-*
-* -------------------------------------------------------------------------
-*
-* IT++ - C++ library of mathematical, signal processing, speech processing,
-*        and communications classes and functions
-*
-* Copyright (C) 1995-2005  (see AUTHORS file for a list of contributors)
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*
-* -------------------------------------------------------------------------
-*/
+ * \file 
+ * \brief Statistical routines test program
+ * \author Tony Ottosson, Adam Piatyszek and Andy Panov
+ *
+ * $Date$
+ * $Revision$
+ *
+ * -------------------------------------------------------------------------
+ *
+ * IT++ - C++ library of mathematical, signal processing, speech processing,
+ *        and communications classes and functions
+ *
+ * Copyright (C) 1995-2005  (see AUTHORS file for a list of contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * -------------------------------------------------------------------------
+ */
 
 #include <itpp/itbase.h>
+#include <iomanip>
 
 using namespace itpp;
 using namespace std;
+
+
+void display_pdf(Histogram<double>& hist)
+{
+  cout.setf(ios::fixed);
+  
+  const int max_asterisks_per_line = 40;
+  ivec bins = hist.get_bins();
+ 
+  // compute and display experimental PDF 
+  vec exp_pdf = hist.get_pdf();
+  double pdf_max = max(exp_pdf);
+
+  cout << "  bin | count |     PDF |" << endl
+       << "------+-------+---------+-------------------------------------------"
+       << endl;
+  for (int i = 0; i < exp_pdf.length(); i++) {
+    int num_asterisks = static_cast<int>(exp_pdf(i) * max_asterisks_per_line 
+					 / pdf_max);
+    cout << setw(5) << setprecision(1) << round_to_zero(hist.get_bin_center(i))
+	 << " | " << setw(5) << hist.get_bin(i) << " | "
+	 << setw(7) << setprecision(5) << round_to_zero(exp_pdf(i)) << " | ";
+    for (int j = 0; j < num_asterisks; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }  
+  cout << "------+-------+---------+-------------------------------------------"
+       << endl;
+  cout << "Histogram trials counter  : " << sum(hist.get_bins()) << endl;
+  cout << "Sum of the histogram bins : " << sum(hist.get_bins()) << endl;
+  cout << "Sum of PDF values         : " << sum(exp_pdf) << endl;
+  cout << "--------------------------------------------------------------------"
+       << endl << endl;
+}
 
 
 int main()
@@ -77,7 +113,45 @@ int main()
   cout << "norm(A) = " << norm(A) << endl;
   cout << "norm(A, 2) = " << norm(A,2) << endl;
   cout << "norm(A, 1) = " << norm(A,1) << endl;
-  cout << "norm(A, \"fro\") = " << norm(A, "fro") << endl;
+  cout << "norm(A, \"fro\") = " << norm(A, "fro") << endl << endl;
+
+  cout << "=======================" << endl;
+  cout << "    Histogram tests    " << endl;
+  cout << "=======================" << endl << endl;
+ 
+  // create histogram
+  Histogram<double> hist(-3, 3, 21);
+ 
+  // matrix dimension for statistical test
+  int mat_dim = 100;
+ 
+  cout << "Experimental PDF of " << mat_dim << "x" << mat_dim
+       << " normal distributed random matrix:" << endl << endl;
+
+  // compute histogram for a random matrix
+  hist.update(randn(mat_dim, mat_dim));
+  display_pdf(hist);
+
+  // reset histogram, so we can start next experiment
+  hist.reset(); 
+
+  // compute histogram for a random vector 
+  int num_stat_trials = 50000;
+ 
+  cout << "Experimental PDF of "<< num_stat_trials
+       << " normal distributed random variables:" << endl << endl;
+  
+  // compute histogram for random vector
+  hist.update(randn(num_stat_trials));
+  display_pdf(hist);
+ 
+  // compute CDF. CDF is computed vs. right bin boundaries
+  cout << "Experimental CDF (CDF(x) = Pr(a < x), a - random variable) " << endl
+       << "of the same vector:" << endl << endl;
+  vec exp_cdf = hist.get_cdf();
+  for (int i = 0; i < exp_cdf.length(); i++)
+    cout << "CDF(" << setw(5) << setprecision(2) << hist.get_bin_right(i) 
+	 << ") = " << setw(6) << setprecision(4) << exp_cdf(i) << endl;
 
   return 0;
 }
