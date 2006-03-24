d81951cf6ef06a38d5eadc8231a0a8b4b83d9f61
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2006-03-24 08:02:54 +0000

    Added missing "itpp/itsignal.h" file
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@386 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/sources.mk
+++ itpp/sources.mk
@@ -6,6 +6,7 @@ h_sources = \
 	$(top_srcdir)/itpp/itfixed.h \
 	$(top_srcdir)/itpp/itmex.h \
 	$(top_srcdir)/itpp/itprotocol.h \
+	$(top_srcdir)/itpp/itsignal.h \
 	$(top_srcdir)/itpp/itsrccode.h
 
 cpp_sources =
