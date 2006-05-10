95ff80be228c65d236811510e2c21bbfbea5ae88
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2006-05-10 14:24:35 +0000

    Minor fixes in test programs due to different formatting under MinGW environment
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@459 a6533b66-300f-0410-b061-b36de6c4fb2d

--- tests/window_test.cpp
+++ tests/window_test.cpp
@@ -43,20 +43,20 @@ int main(void)
  cout << "    Test of window functions    " << endl;
  cout << "================================" << endl;
 
- cout << "hamming(32) = " << hamming(32) << endl;
- cout << "hamming(128) = " << hamming(128) << endl;
+ cout << "hamming(32) = " << round_to_zero(hamming(32)) << endl;
+ cout << "hamming(128) = " << round_to_zero(hamming(128)) << endl;
 
- cout << "hanning(32) = " << hanning(32) << endl;
- cout << "hanning(128) = " << hanning(128) << endl;
+ cout << "hanning(32) = " << round_to_zero(hanning(32)) << endl;
+ cout << "hanning(128) = " << round_to_zero(hanning(128)) << endl;
 
- cout << "hann(32) = " << hann(32) << endl;
- cout << "hann(128) = " << hann(128) << endl;
+ cout << "hann(32) = " << round_to_zero(hann(32)) << endl;
+ cout << "hann(128) = " << round_to_zero(hann(128)) << endl;
 
- cout << "blackman(32) = " << blackman(32) << endl;
- cout << "blackman(128) = " << blackman(128) << endl;
+ cout << "blackman(32) = " << round_to_zero(blackman(32)) << endl;
+ cout << "blackman(128) = " << round_to_zero(blackman(128)) << endl;
 
- cout << "triang(32) = " << triang(32) << endl;
- cout << "triang(128) = " << triang(128) << endl;
+ cout << "triang(32) = " << round_to_zero(triang(32)) << endl;
+ cout << "triang(128) = " << round_to_zero(triang(128)) << endl;
 
  return 0;
 
