20a2d864f11e678fdf77943c0699d8852d472a44
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2006-06-17 22:28:48 +0000

    Modularisation of the IT++ library implemented. It is now possible to skip the building of various modules using a set of `--disable-*' switches with the configure script. The documentation has been updated accordingly.
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@500 a6533b66-300f-0410-b061-b36de6c4fb2d

--- itpp/sources.mk
+++ itpp/sources.mk
@@ -1,11 +1,19 @@
-h_sources = \
-	$(top_builddir)/itpp/config.h \
+h_sources = $(top_builddir)/itpp/config.h \
 	$(top_srcdir)/itpp/itbase.h \
-	$(top_srcdir)/itpp/itcomm.h \
-	$(top_srcdir)/itpp/itfixed.h \
-	$(top_srcdir)/itpp/itmex.h \
-	$(top_srcdir)/itpp/itprotocol.h \
-	$(top_srcdir)/itpp/itsignal.h \
-	$(top_srcdir)/itpp/itsrccode.h
+	$(top_srcdir)/itpp/itmex.h
 
-cpp_sources =
+if ENABLE_COMM
+  h_sources += $(top_srcdir)/itpp/itcomm.h
+endif
+if ENABLE_FIXED
+  h_sources += $(top_srcdir)/itpp/itfixed.h
+endif
+if ENABLE_PROTOCOL
+  h_sources += $(top_srcdir)/itpp/itprotocol.h
+endif
+if ENABLE_SIGNAL
+  h_sources += $(top_srcdir)/itpp/itsignal.h
+endif
+if ENABLE_SRCCODE
+  h_sources += $(top_srcdir)/itpp/itsrccode.h
+endif
