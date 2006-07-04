fe2b47afcc1760e03681a2e754fa4dd692ce277f
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2006-07-04 21:58:49 +0000

    Fixed bug [1516976] by setting the threshold of round_to_zero() function to 1e-13. Thanks to Rosario for reporting this bug.
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@501 a6533b66-300f-0410-b061-b36de6c4fb2d

--- tests/eigen_test.cpp
+++ tests/eigen_test.cpp
@@ -1,34 +1,34 @@
 /*!
-* \file 
-* \brief Eigenvalue decomposition test program
-* \author Tony Ottosson and Adam Piatyszek
-*
-* $Date$
-* $Revision$
-*
-* -------------------------------------------------------------------------
-*
-* IT++ - C++ library of mathematical, signal processing, speech processing,
-*        and communications classes and functions
-*
-* Copyright (C) 1995-2005  (see AUTHORS file for a list of contributors)
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*
-* -------------------------------------------------------------------------
-*/
+ * \file 
+ * \brief Eigenvalue decomposition test program
+ * \author Tony Ottosson and Adam Piatyszek
+ *
+ * $Date$
+ * $Revision$
+ *
+ * -------------------------------------------------------------------------
+ *
+ * IT++ - C++ library of mathematical, signal processing, speech processing,
+ *        and communications classes and functions
+ *
+ * Copyright (C) 1995-2005  (see AUTHORS file for a list of contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * -------------------------------------------------------------------------
+ */
 
 #include <itpp/itbase.h>
 
@@ -40,68 +40,70 @@ using namespace std;
 
 int main(void)
 {
- cout << "================================" << endl;
- cout << "  Test of eigenvalue routines   " << endl;
- cout << "================================" << endl;
+  cout << "================================" << endl;
+  cout << "  Test of eigenvalue routines   " << endl;
+  cout << "================================" << endl;
 
- {
-   cout << "Real symmetric matrix" << endl;
-   mat A = randn(5, 5);
-   A = transpose(A) * A; // make it symmetic
-   mat V;
-   vec d;
-   eig_sym(A, d, V);
+  const double thresh = 1e-13;
 
-   cout << "A = " << round_to_zero(A) << endl;
-   cout << "norm(A*V-V*diag(d)) = " 
-	 << round_to_zero(norm(A * V - V * diag(d))) << endl;
- }
+  {
+    cout << "Real symmetric matrix" << endl;
+    mat A = randn(5, 5);
+    A = transpose(A) * A; // make it symmetic
+    mat V;
+    vec d;
+    eig_sym(A, d, V);
 
- {
-   cout << endl << "Real non-symmetric matrix" << endl;
-   mat A = randn(5, 5);
-   cmat V;
-   cvec d;
-   eig(A, d, V);
+    cout << "A = " << round_to_zero(A) << endl;
+    cout << "norm(A*V-V*diag(d)) = " 
+	 << round_to_zero(norm(A * V - V * diag(d)), thresh) << endl;
+  }
 
-   cout << "A = " << round_to_zero(A) << endl;
-   cout << "norm(A*V-V*diag(d)) = " 
-	 << round_to_zero(norm(A * V - V * diag(d))) << endl;
- }
+  {
+    cout << endl << "Real non-symmetric matrix" << endl;
+    mat A = randn(5, 5);
+    cmat V;
+    cvec d;
+    eig(A, d, V);
 
- {
-   cout << endl << "Complex hermitian matrix" << endl;
-   cmat A = randn_c(5, 5);
-   A = transpose(conj(A)) * A; // make it hermitian
-   cmat V;
-   vec d;
-   eig_sym(A, d, V);
+    cout << "A = " << round_to_zero(A) << endl;
+    cout << "norm(A*V-V*diag(d)) = " 
+	 << round_to_zero(norm(A * V - V * diag(d)), thresh) << endl;
+  }
 
-   cout << "A = " << round_to_zero(A) << endl;
-   cout << "norm(A*V-V*diag(d)) = " 
-	 << round_to_zero(norm(A * V - V * diag(d))) << endl;
- }
+  {
+    cout << endl << "Complex hermitian matrix" << endl;
+    cmat A = randn_c(5, 5);
+    A = transpose(conj(A)) * A; // make it hermitian
+    cmat V;
+    vec d;
+    eig_sym(A, d, V);
 
- {
-   cout << endl << "Complex non-hermitian matrix" << endl;
-   cmat A = randn_c(5, 5);
-   cmat V;
-   cvec d;
-   eig(A, d, V);
+    cout << "A = " << round_to_zero(A) << endl;
+    cout << "norm(A*V-V*diag(d)) = " 
+	 << round_to_zero(norm(A * V - V * diag(d)), thresh) << endl;
+  }
 
-   cout << "A = " << round_to_zero(A) << endl;
-   cout << "norm(A*V-V*diag(d)) = " 
-	 << round_to_zero(norm(A * V - V * diag(d))) << endl;
- }
+  {
+    cout << endl << "Complex non-hermitian matrix" << endl;
+    cmat A = randn_c(5, 5);
+    cmat V;
+    cvec d;
+    eig(A, d, V);
 
- return 0;
+    cout << "A = " << round_to_zero(A) << endl;
+    cout << "norm(A*V-V*diag(d)) = " 
+	 << round_to_zero(norm(A * V - V * diag(d)), thresh) << endl;
+  }
+
+  return 0;
 }
 
 #else
 
 int main() { 
- cerr << "Error: LAPACK library is needed to run this test program" << endl;
- return 1;
+  cerr << "Error: LAPACK library is needed to run this test program" << endl;
+  return 1;
 }
 
 #endif
