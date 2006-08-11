64765acf21aeb75a1707a817ef99a664c3f6fb1c
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2006-08-11 09:00:16 +0000

    Fixed bug [1538210], caused by uninitialised variable `m2' used for covariance calculation. Thanks to Max Moorkamp for reporting this bug.
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@605 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/signal/sigfun.cpp
+++ itpp/signal/sigfun.cpp
@@ -231,26 +231,40 @@ namespace itpp {
 
   mat cov(const mat &X, bool is_zero_mean)
   {
-    int d = X.cols(), n = X.rows(), i, j, k;
+    int d = X.cols(), n = X.rows();
     mat R(d, d), m2(n, d);
     vec tmp;
 
+    R = 0.0;
+
     if (!is_zero_mean) {
       // Compute and remove mean
-      for (i=0; i<d; i++) {
+      for (int i = 0; i < d; i++) {
 	tmp = X.get_col(i);
 	m2.set_col(i, tmp - mean(tmp));
       }
-    }
 
-    // Calc corr matrix
-    R = 0.0;
-    for (i=0; i<d; i++)
-      for (j=0; j<=i; j++) {
-	for (k=0; k<n; k++)
-	  R(i,j) += m2(k,i) * m2(k,j);
-	R(j,i) = R(i,j); // When i=j this is unnecassary work 
+      // Calc corr matrix
+      for (int i = 0; i < d; i++) {
+	for (int j = 0; j <= i; j++) {
+	  for (int k = 0; k < n; k++) {
+	    R(i,j) += m2(k,i) * m2(k,j);
+	  }
+	  R(j,i) = R(i,j); // When i=j this is unnecassary work 
+	}
       }
+    }
+    else {
+      // Calc corr matrix
+      for (int i = 0; i < d; i++) {
+	for (int j = 0; j <= i; j++) {
+	  for (int k = 0; k < n; k++) {
+	    R(i,j) += X(k,i) * X(k,j);
+	  }
+	  R(j,i) = R(i,j); // When i=j this is unnecassary work 
+	}
+      }
+    }
     R /= n;
 
     return R;
