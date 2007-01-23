107a70e3a67eeae0c6cc0dc35350535382dca8c7
ediap <ediap@a6533b66-300f-0410-b061-b36de6c4fb2d>
2007-01-23 08:45:48 +0000

    Merged rev. 860:863 from the itpp-3-10 branch (see ChangeLog file for more info).
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@864 a6533b66-300f-0410-b061-b36de6c4fb2d

---
+++ tests/itfile_test.cpp
@@ -0,0 +1,78 @@
+/*!
+* \file 
+* \brief IT file endianness test program
+* \author Adam Piatyszek
+*
+* $Date$
+* $Revision$
+*
+* -------------------------------------------------------------------------
+*
+* IT++ - C++ library of mathematical, signal processing, speech processing,
+*        and communications classes and functions
+*
+* Copyright (C) 1995-2007  (see AUTHORS file for a list of contributors)
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*
+* -------------------------------------------------------------------------
+*/
+
+#include <itpp/itbase.h>
+
+using namespace itpp;
+using namespace std;
+
+#ifndef ITFILE_TEST_FILE
+
+int main() {
+  cout << "ITFILE_TEST_FILE not defined. Test skipped." << endl;
+}
+
+#else
+
+int main()
+{
+  int x = 1234567890;
+  int y;
+
+  it_file_base::data_header dh;
+
+  it_file ff;
+  ff.open(string(ITFILE_TEST_FILE));
+#ifdef SAVE_DATA  
+  ff << Name("x") << x;
+#endif
+  ff >> Name("x") >> y;
+  ff.close();
+
+  ff.open(string(ITFILE_TEST_FILE));
+  ff.read_data_header(dh);
+  ff.close();
+
+  if (dh.endianity == 0)
+    cout << "Little endian data:" << endl;
+  else if (dh.endianity == 1)
+    cout << "Big endian data:" << endl;
+  else
+    it_error("Endianness error");
+  
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
+
+  return 0;
+}
+
+#endif
