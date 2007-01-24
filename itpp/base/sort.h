08ea9631b4c738bc73200528d1921d63b9f955e2
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2007-01-24 11:31:34 +0000

    Minor documentation improvements
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@878 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- itpp/base/sort.h
+++ itpp/base/sort.h
@@ -11,7 +11,7 @@
  * IT++ - C++ library of mathematical, signal processing, speech processing,
  *        and communications classes and functions
  *
- * Copyright (C) 1995-2006  (see AUTHORS file for a list of contributors)
+ * Copyright (C) 1995-2007  (see AUTHORS file for a list of contributors)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,21 +40,45 @@ namespace itpp {
 
   /*! 
     \relates Vec
-    \brief Sort the the vector in increasing order
+    \brief Sort the \c data vector in increasing order
   */
   template<class T>
-	void sort(Vec<T> &data);
+  void sort(Vec<T> &data)
+  {
+    QS(0,data.size()-1,data);
+  }
 
   /*! 
     \relates Vec
     \brief Return an index vector corresponding to a sorted vector 
-		(increasing order)
+    \c data (increasing order)
   */
   template<class T>
-	ivec sort_index(const Vec<T> &data);
+  ivec sort_index(const Vec<T> &data)
+  {
+    int N=data.length(),i;
+    ivec indexlist(N);
 
+    for(i=0;i<N;i++) {
+      indexlist(i)=i;
+    }
+    QSindex(0,N-1,indexlist,data);
+    return indexlist;
+  }
 
-  template<class T> void QS(int low, int high, Vec<T> &data) {
+  /*! 
+    \relates Vec
+    \brief Quick sort function of a subset of a vector \c data 
+
+    \param low Start index of a subvector to be sorted
+    \param high End index of a subvector to be sorted
+    \param data Data vector, in which a part of it is to be sorted
+
+    \note This function uses recurence.
+  */
+  template<class T>
+  void QS(int low, int high, Vec<T> &data)
+  {
     int plow, phigh;
     T a,test;
 
@@ -64,15 +88,15 @@ namespace itpp {
       phigh=high;
       test=data[phigh];
       while (plow<phigh) {
-				if (test<a) {
-					data[plow]=test;
-					plow++;
-					test=data[plow];
-				} else {
-					data[phigh]=test;
-					phigh--;
-					test=data[phigh];
-				}
+	if (test<a) {
+	  data[plow]=test;
+	  plow++;
+	  test=data[plow];
+	} else {
+	  data[phigh]=test;
+	  phigh--;
+	  test=data[phigh];
+	}
       }
       data[plow]=a;
       QS(low,plow-1,data);
@@ -80,57 +104,49 @@ namespace itpp {
     }
   }
 
+  /*! 
+    \relates Vec
+    \brief Quick sort function, which gives a sorted index vector
+    \c indexlist 
+
+    \param low Start index of a subvector to be sorted
+    \param high End index of a subvector to be sorted
+    \param data Data vector, in which a part of it is to be sorted
+    \param indexlist Result of sorting in the form of sorted indexes
+
+    \note This function uses recurence.
+  */
   template<class T>
-	void sort(Vec<T> &data)
-	{
-		QS(0,data.size()-1,data);
+  void QSindex(int low, int high, ivec &indexlist, const Vec<T> &data)
+  {
+    int plow,phigh,testindex,aindex;
+    T a,test;
+
+    if (high>low) {
+      aindex=indexlist[low];
+      a=data[aindex];
+      plow=low;
+      phigh=high;
+      testindex=indexlist[phigh];
+      test=data[testindex];
+      while (plow<phigh) {
+	if (test<a) {
+	  indexlist[plow]=testindex;
+	  plow++;
+	  testindex=indexlist[plow];
+	  test=data[testindex];
+	} else {
+	  indexlist[phigh]=testindex;
+	  phigh--;
+	  testindex=indexlist[phigh];
+	  test=data[testindex];
 	}
-
-  template<class T>
-	void QSindex(int low, int high, ivec &indexlist, const Vec<T> &data)
-	{
-		int plow,phigh,testindex,aindex;
-		T a,test;
-
-		if (high>low) {
-			aindex=indexlist[low];
-			a=data[aindex];
-			plow=low;
-			phigh=high;
-			testindex=indexlist[phigh];
-			test=data[testindex];
-			while (plow<phigh) {
-				if (test<a) {
-					indexlist[plow]=testindex;
-					plow++;
-					testindex=indexlist[plow];
-					test=data[testindex];
-				} else {
-					indexlist[phigh]=testindex;
-					phigh--;
-					testindex=indexlist[phigh];
-					test=data[testindex];
-				}
-			}
-			indexlist[plow]=aindex;
-			QSindex(low,plow-1,indexlist,data);
-			QSindex(plow+1,high,indexlist,data);
-		}
-	}
-
-  template<class T>
-	ivec sort_index(const Vec<T> &data)
-	{
-		int N=data.length(),i;
-		ivec indexlist(N);
-
-		for(i=0;i<N;i++) {
-			indexlist(i)=i;
-		}
-		QSindex(0,N-1,indexlist,data);
-		return indexlist;
-	}
-
+      }
+      indexlist[plow]=aindex;
+      QSindex(low,plow-1,indexlist,data);
+      QSindex(plow+1,high,indexlist,data);
+    }
+  }
 
 } // namespace itpp
 
