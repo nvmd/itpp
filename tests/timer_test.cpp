2dc9e960a37db0db6c5d722a02c7ad02bdf99dd2
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2007-04-10 07:14:18 +0000

    Merged rev. 993:994 from the itpp-3-10 branch (see the ChangeLog file for more info)
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@995 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- tests/timer_test.cpp
+++ tests/timer_test.cpp
@@ -1,34 +1,34 @@
 /*!
-* \file 
-* \brief Timer classes test program
-* \author Thomas Eriksson, Tony Ottosson, Tobias Ringstrom and Adam Piatyszek
-*
-* $Date$
-* $Revision$
-*
-* -------------------------------------------------------------------------
-*
-* IT++ - C++ library of mathematical, signal processing, speech processing,
-*        and communications classes and functions
-*
-* Copyright (C) 1995-2006  (see AUTHORS file for a list of contributors)
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*
-* -------------------------------------------------------------------------
-*/
+ * \file 
+ * \brief Timer classes test program
+ * \author Thomas Eriksson, Tony Ottosson, Tobias Ringstrom and Adam Piatyszek
+ *
+ * $Date$
+ * $Revision$
+ *
+ * -------------------------------------------------------------------------
+ *
+ * IT++ - C++ library of mathematical, signal processing, speech processing,
+ *        and communications classes and functions
+ *
+ * Copyright (C) 1995-2007  (see AUTHORS file for a list of contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * -------------------------------------------------------------------------
+ */
 
 #include <itpp/itbase.h>
 
@@ -37,29 +37,35 @@ using namespace itpp;
 
 int main()
 {
-   CPU_Timer t1;
-   Real_Timer t2;
+  const double period = 2.0;
+  const double relative_error = 0.05;
+  CPU_Timer t1;
+  Real_Timer t2;
 
-   tic();
-   t1.start();
-   t2.start();
+  t1.start();
+  while (t1.get_time() < period) ;
+  t1.stop();
 
-   while (t2.get_time() <= 4.0)
-     ;
+  tic();
+  t2.start();
+  while (t2.get_time() < period) ;
+  t2.stop();
+  double t3 = toc();
 
-   t1.stop();
-   t2.stop();
-   double t3 = toc();
+  if (fabs(t1.get_time() - period) <= relative_error * period)
+    cout << "CPU_Timer is OK" << endl;
+  else
+    cout << "CPU_Timer difference: " << fabs(t1.get_time() - period) 
+	 << " > " << relative_error * period << endl; 
 
-   if (t2.get_time() >= 4.0)
-     cout << "Real_Timer is OK" << endl;
+  if (fabs(t2.get_time() - period) <= relative_error * period)
+    cout << "Real_Timer is OK" << endl;
+  else
+    cout << "Real_Timer difference: " << fabs(t1.get_time() - period) 
+	 << " > " << relative_error * period << endl; 
 
-   // Check against 1 ms margin
-   if (t1.get_time() - t2.get_time() <= 1e-3)
-     cout << "CPU_Timer is OK" << endl;
+  if (t3 >= t2.get_time())
+    cout << "tic() and toc() are OK" << endl;
 
-   if (t3 >= t2.get_time())
-     cout << "tic() and toc() is OK" << endl;
-
-   return 0;
+  return 0;
 }
