99462a0de59095b990d8c61d23a5922b1bb19698
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2007-10-16 08:59:01 +0000

    Changed the way version numbers are handled
    This patch fixes the problem of removing the "itpp.spec" and
    "itpp-config.1" files during "make distclean". These files should
    not be removed, since they are distributed in packages. Since now,
    version numbers in these two files and in "configure.ac" are
    updated according to a VERSION file content, when "autogen.sh"
    bootstrapping script is executed.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@1244 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+[ -f configure.ac.in ] || {
+	echo "'./autogen.sh' has to be run in the IT++ source directory";
+	echo;
+	exit;
+}
+
 (libtoolize --version) < /dev/null > /dev/null 2>&1 || {
 	echo "You must have libtool installed to compile IT++";
 	echo;
@@ -17,6 +23,17 @@
 	echo;
 	exit;
 }
+
+PV="`cat VERSION | cut -d' ' -f1`"
+LV="`cat VERSION | cut -d' ' -f2`"
+PD="`LC_ALL=C date +\"%B %Y\"`"
+
+sed -e "s/@PACKAGE_VERSION@/${PV}/" -e "s/@LIBRARY_VERSION@/${LV}/" \
+	< configure.ac.in > configure.ac || exit;
+sed -e "s/@PACKAGE_VERSION@/${PV}/" \
+	< itpp.spec.in > itpp.spec || exit;
+sed -e "s/@PACKAGE_VERSION@/${PV}/" -e "s/@PACKAGE_DATE@/${PD}/" \
+	< itpp-config.1.in > itpp-config.1 || exit;
 
 aclocal -I config || exit;
 libtoolize --copy --force --automake || exit;
