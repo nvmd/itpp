5cfaafc67614885152ab5416b3048b126845a4b8
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2008-01-01 20:27:45 +0000

    License change: GPLv2 -> GPLv3
    BTW, update Copyright years to `1995-2008'
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@1391 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- tests/pulse_shape_test.cpp
+++ tests/pulse_shape_test.cpp
@@ -1,33 +1,31 @@
 /*!
-* \file
-* \brief Pulse shaping classes test program
-* \author Adam Piatyszek
-*
-* -------------------------------------------------------------------------
-*
-* IT++ - C++ library of mathematical, signal processing, speech processing,
-*        and communications classes and functions
-*
-* Copyright (C) 1995-2007  (see AUTHORS file for a list of contributors)
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*
-* -------------------------------------------------------------------------
-*/
+ * \file
+ * \brief Pulse shaping classes test program
+ * \author Adam Piatyszek
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 1995-2008  (see AUTHORS file for a list of contributors)
+ *
+ * This file is part of IT++ - a C++ library of mathematical, signal
+ * processing, speech processing, and communications classes and functions.
+ *
+ * IT++ is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * IT++ is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with IT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
 
-#include <itpp/itbase.h>
 #include <itpp/itcomm.h>
 
 using namespace std;
