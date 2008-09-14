694f9f5133fe67894ef33b3dcb7567a13a10180e
ediap <ediap@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2008-09-14 10:25:29 +0000

    Add specmat_test program for testing special vectors and matrices.
    Initially only toeplitz() function is tested.
    
    Signed-off-by: Adam Piątyszek <ediap@users.sourceforge.net>
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@1629 f17a810d-92cc-4fca-aeb8-6e32867107aa

---
+++ tests/specmat_test.cpp
@@ -0,0 +1,67 @@
+/*!
+ * \file
+ * \brief Test program of special vectors and matrices
+ * \author Adam Piatyszek
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 1995-2008  (see AUTHORS file for a list of contributors)
+ *
+ * This file is part of IT++ - a C++ library of mathematical, signal
+ * processing, speech processing, and communications classes and functions.
+ *
+ * IT++ is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * IT++ is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with IT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#include <itpp/itbase.h>
+#include <iomanip>
+
+using namespace std;
+using namespace itpp;
+
+int main()
+{
+  cout << "=================================" << endl;
+  cout << "    Test of specmat routines" << endl;
+  cout << "=================================" << endl;
+
+  cout.setf(ios::fixed);
+  cout.precision(3);
+
+  bvec b1 = randb(7);
+  bvec b2 = randb(13);
+  cout << "b1 = " << b1 << endl;
+  cout << "b2 = " << b2 << endl;
+  cout << "toeplitz(b1, b2) =" << endl << toeplitz(b1, b2) << endl;
+  cout << "toeplitz(b1) =" << endl << toeplitz(b1) << endl << endl;
+
+  vec v1 = randn(5);
+  vec v2 = randn(7);
+  cout << "v1 = " << v1 << endl;
+  cout << "v2 = " << v2 << endl;
+  cout << "toeplitz(v1, v2) =" << endl << toeplitz(v1, v2) << endl;
+  cout << "toeplitz(v1) =" << endl << toeplitz(v1) << endl << endl;
+
+  cvec c1 = randn_c(4);
+  cvec c2 = randn_c(3);
+  cout << "c1 = " << c1 << endl;
+  cout << "c2 = " << c2 << endl;
+  cout << "toeplitz(c1, c2) =" << endl << toeplitz(c1, c2) << endl;
+  cout << "toeplitz(c1, c1) =" << endl << toeplitz(c1, c1) << endl;
+  cout << "toeplitz(c1) =" << endl << toeplitz(c1) << endl << endl;
+
+  return 0;
+}
