7406b7001303bb4ddc2aa5be06f06489d35c8b96
cristeab <cristeab@a6533b66-300f-0410-b061-b36de6c4fb2d>
2010-02-22 20:11:35 +0000

    changed bessel_test.cpp for compatibility reasons with Windows
    git-svn-id: https://itpp.svn.sourceforge.net/svnroot/itpp/itpp/trunk@1767 a6533b66-300f-0410-b061-b36de6c4fb2d

--- tests/bessel_test.cpp
+++ tests/bessel_test.cpp
@@ -1,4 +1,5 @@
 #include <itpp/itbase.h>
+#include <iomanip>
 
 using namespace std;
 using namespace itpp;
@@ -15,29 +16,29 @@ int main(void)
   cout << "x = " << x << endl;
 
   cout << "besselj(0, x) = " << fixed << besselj(0, x) << endl;
-  cout << "besselj(1, x) = " << fixed << besselj(1, x) << endl;
-  cout << "besselj(5, x) = " << fixed << besselj(5, x) << endl;
-  cout << "besselj(0.3, x) = " << fixed << besselj(0.3, x) << endl;
-  cout << "besselj(1.7, x) = " << fixed << besselj(1.7, x) << endl;
-  cout << "besselj(5.3, x) = " << fixed << besselj(5.3, x) << endl;
+  cout << "besselj(1, x) = " << besselj(1, x) << endl;
+  cout << "besselj(5, x) = " << besselj(5, x) << endl;
+  cout << "besselj(0.3, x) = " << besselj(0.3, x) << endl;
+  cout << "besselj(1.7, x) = " << besselj(1.7, x) << endl;
+  cout << "besselj(5.3, x) = " << besselj(5.3, x) << endl;
 
-  cout << "bessely(0, x) = " << fixed << bessely(0, x) << endl;
-  cout << "bessely(1, x) = " << fixed << bessely(1, x) << endl;
-  cout << "bessely(5, x) = " << fixed << bessely(5, x) << endl;
-  cout << "bessely(0.3, x) = " << fixed << bessely(0.3, x) << endl;
-  cout << "bessely(1.7, x) = " << fixed << bessely(1.7, x) << endl;
-  cout << "bessely(5.3, x) = " << fixed << bessely(5.3, x) << endl;
+  cout << "bessely(0, x) = " << bessely(0, x) << endl;
+  cout << "bessely(1, x) = " << bessely(1, x) << endl;
+  cout << "bessely(5, x) = " << setprecision(3) << bessely(5, x) << endl;
+  cout << "bessely(0.3, x) = " << setprecision(6) << bessely(0.3, x) << endl;
+  cout << "bessely(1.7, x) = " << bessely(1.7, x) << endl;
+  cout << "bessely(5.3, x) = " << setprecision(3) << bessely(5.3, x) << endl;
 
-  cout << "besseli(0, x) = " << fixed << besseli(0, x) << endl;
-  cout << "besseli(1, x) = " << fixed << besseli(1, x) << endl;
-  cout << "besseli(5, x) = " << fixed << besseli(5, x) << endl;
-  cout << "besseli(0.3, x) = " << fixed << besseli(0.3, x) << endl;
-  cout << "besseli(1.7, x) = " << fixed << besseli(1.7, x) << endl;
-  cout << "besseli(5.3, x) = " << fixed << besseli(5.3, x) << endl;
+  cout << "besseli(0, x) = " << setprecision(6) << besseli(0, x) << endl;
+  cout << "besseli(1, x) = " << besseli(1, x) << endl;
+  cout << "besseli(5, x) = " << besseli(5, x) << endl;
+  cout << "besseli(0.3, x) = " << besseli(0.3, x) << endl;
+  cout << "besseli(1.7, x) = " << besseli(1.7, x) << endl;
+  cout << "besseli(5.3, x) = " << besseli(5.3, x) << endl;
 
-  cout << "besselk(0, x) = " << fixed << besselk(0, x) << endl;
-  cout << "besselk(1, x) = " << fixed << besselk(1, x) << endl;
-  cout << "besselk(5, x) = " << fixed << besselk(5, x) << endl;
+  cout << "besselk(0, x) = " << besselk(0, x) << endl;
+  cout << "besselk(1, x) = " << besselk(1, x) << endl;
+  cout << "besselk(5, x) = " << setprecision(3) << besselk(5, x) << endl;
 
   return 0;
 
