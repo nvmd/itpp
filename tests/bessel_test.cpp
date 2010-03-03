b26c6cd66a7ea48bde0570492d8226d562ed4fff
cristeab <cristeab@f17a810d-92cc-4fca-aeb8-6e32867107aa>
2010-03-03 19:52:40 +0000

    changed two tests in order to pass test checking
    git-svn-id: svn+ssh://svn.code.sf.net/p/itpp/svn/itpp/trunk@1771 f17a810d-92cc-4fca-aeb8-6e32867107aa

--- tests/bessel_test.cpp
+++ tests/bessel_test.cpp
@@ -1,8 +1,55 @@
+/*!
+ * \file
+ * \brief Bessel test program
+ * \author Tony Ottosson
+ *
+ * -------------------------------------------------------------------------
+ *
+ * Copyright (C) 1995-2010  (see AUTHORS file for a list of contributors)
+ *
+ * This file is part of IT++ - a C++ library of mathematical, signal
+ * processing, speech processing, and communications classes and functions.
+ *
+ * IT++ is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * IT++ is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with IT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
 #include <itpp/itbase.h>
 #include <iomanip>
 
 using namespace std;
 using namespace itpp;
+
+inline vec round_to_infty(const vec &in, const double threshold = 1e6)
+//this function is used to remove trailing digits, found after the decimal point
+//for numbers greater than threshold
+{
+   int in_len = in.length();
+   vec out(in_len);
+   for(int n=0; n<in_len; n++)
+   {
+      if(std::fabs(in(n))>threshold)
+      {
+         out(n) = itpp::round(in(n));
+      } else
+      {
+         out(n) = in(n);
+      }
+   }
+   return out;
+}
 
 int main(void)
 {
@@ -17,19 +64,19 @@ int main(void)
 
   cout << "besselj(0, x) = " << fixed << besselj(0, x) << endl;
   cout << "besselj(1, x) = " << besselj(1, x) << endl;
-  cout << "besselj(5, x) = " << besselj(5, x) << endl;
+  cout << "besselj(5, x) = " << round_to_infty(besselj(5, x)) << endl;
   cout << "besselj(0.3, x) = " << besselj(0.3, x) << endl;
   cout << "besselj(1.7, x) = " << besselj(1.7, x) << endl;
-  cout << "besselj(5.3, x) = " << besselj(5.3, x) << endl;
+  cout << "besselj(5.3, x) = " << round_to_infty(besselj(5.3, x)) << endl;
 
   cout << "bessely(0, x) = " << bessely(0, x) << endl;
   cout << "bessely(1, x) = " << bessely(1, x) << endl;
-  cout << "bessely(5, x) = " << setprecision(3) << bessely(5, x) << endl;
-  cout << "bessely(0.3, x) = " << setprecision(6) << bessely(0.3, x) << endl;
+  cout << "bessely(5, x) = " << round_to_infty(bessely(5, x)) << endl;
+  cout << "bessely(0.3, x) = " << bessely(0.3, x) << endl;
   cout << "bessely(1.7, x) = " << bessely(1.7, x) << endl;
-  cout << "bessely(5.3, x) = " << setprecision(3) << bessely(5.3, x) << endl;
+  cout << "bessely(5.3, x) = " << round_to_infty(bessely(5.3, x)) << endl;
 
-  cout << "besseli(0, x) = " << setprecision(6) << besseli(0, x) << endl;
+  cout << "besseli(0, x) = " << besseli(0, x) << endl;
   cout << "besseli(1, x) = " << besseli(1, x) << endl;
   cout << "besseli(5, x) = " << besseli(5, x) << endl;
   cout << "besseli(0.3, x) = " << besseli(0.3, x) << endl;
@@ -38,7 +85,7 @@ int main(void)
 
   cout << "besselk(0, x) = " << besselk(0, x) << endl;
   cout << "besselk(1, x) = " << besselk(1, x) << endl;
-  cout << "besselk(5, x) = " << setprecision(3) << besselk(5, x) << endl;
+  cout << "besselk(5, x) = " << round_to_infty(besselk(5, x)) << endl;
 
   return 0;
 
