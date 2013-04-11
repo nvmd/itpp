3f7d5dd564e2991fa4ecb89d6686f8101506b89b
Bogdan Cristea <cristeab@gmail.com>
2013-04-11 13:28:31 +0200

    corr warnings in fastica_test with MSVC

--- gtests/fastica_test.cpp
+++ gtests/fastica_test.cpp
@@ -71,6 +71,9 @@
  * - optimization of SVD decomposition (performed 2 times in Matlab, only 1 time in IT++) *
  * - default approach is SYMM wit non-linearity POW3                                      *
  *----------------------------------------------------------------------------------------*/
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include <itpp/itsignal.h>
 #include <cstdio>
