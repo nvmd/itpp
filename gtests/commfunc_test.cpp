e075f782288271d433e8ce8c4758abb3073f247f
Bogdan Cristea <cristeab@gmail.com>
2013-05-24 08:18:27 +0200

    corrections for cygwin

--- gtests/commfunc_test.cpp
+++ gtests/commfunc_test.cpp
@@ -34,6 +34,8 @@ using namespace itpp;
 
 TEST(CommFunc, All)
 {
+  const double tol = 1e-9;
+
   bmat expected_gray = "0 0 0 0 0;\
                         0 0 0 0 1;\
                         0 0 0 1 1;\
@@ -78,36 +80,36 @@ TEST(CommFunc, All)
   vec actual_water = waterfilling("1 2 3", 0.001);
   int i = 0;
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "0 0 0.010000000000000009";
   actual_water = waterfilling("1 2 3", 0.01);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "0 0 0.1";
   actual_water = waterfilling("1 2 3", 0.1);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "0 0.16666666666666663 0.33333333333333331";
   actual_water = waterfilling("1 2 3", 0.5);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "0 0.41666666666666652 0.58333333333333326";
   actual_water = waterfilling("1 2 3", 1);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "1.2777777777777777 1.7777777777777777 1.9444444444444444";
   actual_water = waterfilling("1 2 3", 5);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
   expected_water = "32.944444444444443 33.444444444444443 33.611111111111107";
   actual_water = waterfilling("1 2 3", 100);
   for (i = 0; i < actual_water.length(); ++i) {
-    ASSERT_DOUBLE_EQ(expected_water[i], actual_water[i]);
+    ASSERT_NEAR(expected_water[i], actual_water[i], tol);
   }
 }
